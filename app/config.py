77dbac86a61973309cb0e6fe32aee2028255cf97
Prabhu-Jethi <prabhujethi9@gmail.com>
2026-09-15 12:32:06 +0530

    config file updation

--- app/config.py
+++ app/config.py
@@ -5,9 +5,13 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ## from .env 
     DATABASE_URL: str
+    SUPABASE_URL: str
+    SUPABASE_PUBLISHABLE_KEY: str
+    SUPABASE_SECRET_KEY: str
+    SUPABASE_JWKS_URL: str
 
-class Config:
-    env_file = ".env"
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
